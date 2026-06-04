511517d95575933946c55e9cdc12df64500ff806
manish1730 <manishchoudary47@gmail.com>
2026-06-04 11:19:24 +0530

    fixed the backend login

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,21 @@
+import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoutes);
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("School CRM Backend Running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
